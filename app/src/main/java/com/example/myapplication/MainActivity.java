51f1b2dfab565b38dd909b1011d708ba66510db1
alimovjamshid <jamshidalimov3464@gmail.com>
2022-08-18 19:14:28 +0500

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
@@ -13,26 +14,37 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    float speed=0.6f;
     TextToSpeech t1;
     EditText ed1;
     Button b1;
     Button b2;
+    Locale language=Locale.US;
+    Button slow;
+    Button normal;
+    Button fast;
+    Button american;
+    Button british;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ed1=(EditText)findViewById(R.id.editText);
-        b1=(Button)findViewById(R.id.button);
-        b2=(Button)findViewById(R.id.button2);
+        b1=(Button)findViewById(R.id.speech);
+        b2=(Button)findViewById(R.id.clear);
+        slow=(Button)findViewById(R.id.slow);
+        normal=(Button)findViewById(R.id.normal);
+        fast=(Button)findViewById(R.id.fast);
+        american=(Button)findViewById(R.id.american);
+        british=(Button)findViewById(R.id.british);
 
         t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if(status != TextToSpeech.ERROR) {
-                    t1.setLanguage(Locale.ENGLISH);
-                    t1.setSpeechRate(0.3f);
-
+                    t1.setLanguage(language);
+                    t1.setSpeechRate(speed);
                 }
             }
         });
@@ -41,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String toSpeak = ed1.getText().toString();
-                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
                 t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
             }
         });
@@ -52,6 +63,42 @@ public class MainActivity extends AppCompatActivity {
                 ed1.setText("");
             }
         });
+
+        slow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speed=0.3f;
+                t1.setSpeechRate(speed);
+            }
+        });
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speed=0.9f;
+                t1.setSpeechRate(speed);
+            }
+        });
+        fast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speed=1.2f;
+                t1.setSpeechRate(speed);
+            }
+        });
+        american.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                language=Locale.US;
+                t1.setLanguage(language);
+            }
+        });
+        british.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                language=Locale.UK;
+                t1.setLanguage(language);
+            }
+        });
     }
 
     @Override
@@ -60,7 +107,20 @@ public class MainActivity extends AppCompatActivity {
             t1.stop();
             t1.shutdown();
         }
-
         super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(language);
+                    t1.setSpeechRate(speed);
+                }
+            }
+        });
+        super.onResume();
     }
 }
